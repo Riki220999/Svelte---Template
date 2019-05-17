a1209cd031fcaa64bb16a232a1d792a7faaae794
Riki setiyo Pambudi <setiyoriki589@gmail.com>
2019-05-18 06:12:11 +0700

    Svelte boiler

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+<body
+  style="background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);">
+  <h1>About The Svelte</h1>
+
+  <p>
+    Svelte merupakan teknologi terbaru untuk membangun User Interface. Sementara
+    React,Vue,Angular,Dll. Bekerja di browser, Svelte merupakan compiler untuk
+    membangun itu.
+  </p>
+  <p>
+    Svelte is a radical new approach to building user interfaces. Whereas
+    traditional frameworks like React and Vue do the bulk of their work in the
+    browser, Svelte shifts that work into a compile step that happens when you
+    build your app.
+  </p>
+</body>
---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<style>
+  h1,
+  figure,
+  body {
+    text-align: center;
+    margin: 0 auto;
+    background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  h1 {
+    padding-bottom: 20px;
+    font-weight: bold;
+  }
+
+  @media (min-width: 480px) {
+  }
+</style>
+
+<svelte:head>
+  <title>The Fast Framework</title>
+</svelte:head>
+
+<body>
+  <h1>You Know... Everything is gone be fine!</h1>
+  <figure>
+    <img alt="Ricky" src="riki.png" />
+    <figcaption>HIGH TECHNOLOGY!!!</figcaption>
+  </figure>
+</body>
